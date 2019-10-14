bb098c3072f4b68df541e248c8db49a518d8a589
Felipe Velandia <fvelandia695@gmail.com>
2019-10-14 17:47:04 -0500

    Add files via upload

--- Venta de ropa.py
+++ Venta de ropa.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 
 class Prenda:
         
@@ -19,6 +19,9 @@ class Camiseta(Prenda):
     def getColor(self):
         return self.color
     
+    def getPrenda(self):
+        return "Camiseta"
+    
 class Pantalon(Prenda):
     
     def __init__(self, color):
@@ -29,6 +32,9 @@ class Pantalon(Prenda):
     
     def getColor(self):
         return self.color
+    
+    def getPrenda(self):
+        return "Pantalon"
     
 class Zapatos(Prenda):
     
@@ -41,52 +47,75 @@ class Zapatos(Prenda):
     def getColor(self):
         return self.color
     
-class Ventana:
-    
-    def __init__(self, master):
-        frame = Frame(master)
-        master.title("Tienda de ropa")
-        frame.pack()
-        color= StringVar()
-        self.carrito = list()
-        self.azul = Radiobutton (frame, text="Azul", variable=color, value="Azul")
-        self.azul.pack()
-        self.rojo = Radiobutton (frame, text="Rojo", variable=color, value="Rojo")
-        self.rojo.pack()
-        self.verde = Radiobutton (frame, text="Verde", variable=color, value="Verde")
-        self.verde.pack()
-        self.negro = Radiobutton (frame, text="Negro", variable=color, value="Negro")
-        self.negro.pack()
-        self.camisa = Button(frame, text="Camiseta", command=self.añadir("Camiseta", color))
-        self.camisa.pack(side=LEFT)
-        self.pantalon = Button(frame, text="Pantalon", command=self.añadir("Pantalon", color))
-        self.pantalon.pack(side=LEFT)
-        self.zapatos = Button(frame, text="Zapatos", command=self.añadir("Zapatos", color))
-        self.zapatos.pack(side=RIGHT)
-        self.comprar = Button(frame, text="Finalizar compra", command=self.total())
-        self.comprar.pack(side=BOTTOM)
-        
-    def añadir(self, prenda, color):
-        if prenda=="Camiseta" :
-            objeto = Camiseta(color)
-            print("Se añadio una camiseta color"+color)
-        elif prenda == "Pantalon":
-            objeto = Pantalon(color)
-            print ("Se añadio un pantalon color "+color)
-        elif prenda == "Zapatos" :
-            objeto = Zapatos(color)
-            print("Se añadieron unos zapatos color "+color)
-        
-        self.carrito.append(objeto)
-        
-    def total(self):
-        total = 0
-        for i in self.carrito:
-            total = total + i.getPrecio()
-        return total
-    
-'''--------------------------------------------------------------------'''
+    def getPrenda(self):
+        return "Zapatos"
 
-root = Tk()
-app = Ventana(root)
+carrito=[]
+
+def añadirCamiseta():
+    color=colores[v.get()]
+    objeto = Camiseta(color)
+    print("Se añadio una camiseta color "+color)
+    carrito.append(objeto)
+    
+def añadirPantalon():
+    color=colores[v.get()]
+    objeto = Pantalon(color)
+    print ("Se añadio un pantalon color "+color)
+    carrito.append(objeto)
+        
+def añadirZapatos():
+    color=colores[v.get()]
+    objeto = Zapatos(color)
+    print("Se añadieron unos zapatos color "+color)
+    carrito.append(objeto)
+ 
+def total():
+    total = 0
+    for i in carrito:
+        total = total + i.getPrecio()
+    print(str(total))
+    factura = tk.Tk()
+    for i in range(len(carrito)):
+        tk.Label(factura,
+                 text=carrito[i].getPrenda()+" "+str(carrito[i].getColor())+"\t"+str(carrito[i].getPrecio())).pack()
+    tk.Label(factura,
+             text="Total: "+str(total)).pack()
+    carrito.clear()
+    
+root = tk.Tk()
+
+v = tk.IntVar()
+v.set(0)
+
+colores = [
+    "Rojo","Verde","Azul","Amarillo"
+]
+
+def ShowChoice():
+    print(v.get())
+
+tk.Label(root, 
+         text="""Seleccione el color de su prenda:""",
+         justify = tk.LEFT,
+         padx = 20).pack()
+
+for val, color in enumerate(colores):
+    tk.Radiobutton(root, 
+                  text=color,
+                  padx = 20, 
+                  variable=v,
+                  value=val).pack(anchor=tk.W)
+    
+
+camisa = Button(root, text="Camiseta", command=añadirCamiseta)
+camisa.pack(side=LEFT)
+pantalon = Button(root, text="Pantalon", command=añadirPantalon)
+pantalon.pack(side=LEFT)
+zapatos = Button(root, text="Zapatos", command=añadirZapatos)
+zapatos.pack(side=RIGHT)
+comprar = Button(root, text="Finalizar compra", command=total)
+comprar.pack(side=BOTTOM)
+
+
 root.mainloop()
